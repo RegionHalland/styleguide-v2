1456c7c80c48a99a547f4248ff69db27d50d72da
David Öhlin <hello@davidohl.in>
2018-10-03 15:22:28 +0200

    wip: build css file

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+	entry: './src/css/main.css',
+	mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader', options: { importLoaders: 1 } },
+            'postcss-loader',
+          ],
+        }),
+      },
+    ],
+  },
+	plugins: [
+	    new ExtractTextPlugin('main.min.css'),
+	]
+}
